f7c77f504787407a2362d8dc6a362457fc609954
GuanRu <riley60530@gmail.com>
2023-07-18 14:20:14 +0800

    新增彈跳式視窗

--- order/order-update.php
+++ order/order-update.php
@@ -33,24 +33,6 @@ $row = $result->fetch_assoc();
 </head>
 
 <body>
-    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="" aria-hidden="true">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="">訊息</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    </button>
-                </div>
-                <div class="modal-body">
-                    確認取消訂單?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
-                    <a href="doDelete.php?id=<?= $id ?>" class="btn btn-danger">確認</a>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="container ">
         <div class="py-3 text-start">
@@ -61,14 +43,41 @@ $row = $result->fetch_assoc();
 
         <div class="my-3">
             <?php
+            // 讓出貨跟取消可以跳出不同底色的message
             if (isset($_GET["message"])) {
                 $message = $_GET["message"];
-                echo '<div class="alert alert-danger">' . $message . '</div>';
+                if ($row["status_id"] == 2) {
+                    echo '<div class="alert alert-warning">' . $message . '</div>';
+                } else if ($row["status_id"] == 4) {
+                    echo '<div class="alert alert-danger">' . $message . '</div>';
+                }
             }
             ?>
         </div>
 
         <form action="order-doUpdate.php" method="post">
+            <!-- 本來彈跳式視窗是在body的最上方 移到這是為了包在form裡面 -->
+            <!-- 將確認鍵的type從button改至submit 且要將原本在取消訂單按鈕的name跟value也加上去 才能連到doUpdate並執行 -->
+            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="" aria-hidden="true">
+                <div class="modal-dialog modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="">訊息</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            確認取消訂單?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
+                            <button type="submit" class="btn btn-danger" name="action" value="cancel">確認</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
             <table class="table table-bordered my-5 text-center">
                 <input type="hidden" name="order_id" value="<?= $order_id ?>">
                 <tr>
@@ -92,19 +101,17 @@ $row = $result->fetch_assoc();
                     <td>$<?= $row["total_amount"] ?></td>
                 </tr>
             </table>
-            <!-- <div class="py-2 d-flex justify-content-center ">
-                <button class="btn btn-dark mx-2" type="submit">出貨</button>
-                <button class="btn btn-danger mx-2" type="button" data-bs-toggle="modal" data-bs-target="#deleteModal">取消訂單</button>
-            </div> -->
 
+            <!-- 將取消訂單的type從submit改至button 才能跳出彈跳式視窗 並將submit移至彈跳式視窗的確認鍵 -->
             <div class="py-2 d-flex justify-content-end">
                 <?php if ($row["status_id"] == 1) : ?>
                     <button class="btn btn-warning mx-2" type="submit" name="action" value="ship">出貨</button>
-                    <button class="btn btn-danger mx-2" type="submit" name="action" value="cancel">取消訂單</button>
+                    <button class="btn btn-danger mx-2" type="button" name="action" value="cancel" data-bs-toggle="modal" data-bs-target="#deleteModal">取消訂單</button>
+
                 <?php elseif ($row["status_id"] == 2 || $row["status_id"] == 3) : ?>
-                    <button class="btn btn-danger mx-2" type="submit"   name="action" value="cancel" >取消訂單</button>
+                    <button class="btn btn-danger mx-2" type="button" name="action" value="cancel" data-bs-toggle="modal" data-bs-target="#deleteModal">取消訂單</button>
                 <?php elseif ($row["status_id"] == 4) : ?>
-                    <a class="btn btn-light mx-2" href="./order-list.php">訂單已取消，回訂單列表</a>
+                    <a class="btn btn-secondary mx-2" href="./order-list.php">訂單已取消，回訂單列表</a>
                 <?php endif; ?>
             </div>
         </form>
@@ -114,7 +121,16 @@ $row = $result->fetch_assoc();
 
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <!-- 因為要套到dashboard裡 而dashboard裡已經有include bootstrap的JS 重複貼的話會衝突跑不出來 所以這邊先註解掉 沒套dashboard時沒跳出彈跳式視窗是正常的 -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+
+    <!-- <script>
+        // 彈跳視窗
+        document.addEventListener('DOMContentLoaded', function() {
+            var myModal = new bootstrap.Modal(document.getElementById('deleteModal'));
+        });
+    </script> -->
+
 </body>
 
 </html>
